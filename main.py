07e4a1fd7cce32c33296b490c7058f0f6dfe1fc5
omeraydin6170-sudo <omeraydin6170@gmail.com>
2026-05-16 04:32:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ except Exception as e:
 class AsistanApp(App):
     def build(self):
         self.tts = None
-        self.api_key = "YOUR_GEMINI_API_KEY" # <--- KENDİ GEMINI KEY'İNİ BURAYA YAPIŞTIR
+        self.api_key = "AIzaSyBi5-oS8jxZJDhZxljhpMVoalWFQ9UxfCw" # <--- KENDİ GEMINI KEY'İNİ BURAYA YAPIŞTIR
         
         # Ana Dikey Arayüz
         main_layout = BoxLayout(orientation='vertical', padding=15, spacing=15)
